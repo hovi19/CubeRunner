af2545ec02598222e0057a2cbdc48ed5057496ae
hovi19 <hovi.viktor@gmail.com>
2020-01-16 18:56:49 +0100

    game

---
+++ 3DCube/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool GameHasEnded = false;
+    public GameObject completelvlUI;
+
+    public void CompleteLevel()
+    {
+        completelvlUI.SetActive(true);
+    }
+
+
+    public void EndGame()
+    {
+        if (GameHasEnded==false)
+        {
+            GameHasEnded = true;
+            Debug.Log("GAME OVER");
+            Invoke("Restart", 2f);
+        }
+       
+    }
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name) ;
+    }
+
+   
+}
